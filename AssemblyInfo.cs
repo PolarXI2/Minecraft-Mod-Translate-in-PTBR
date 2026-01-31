06902bb1f3ce01162445816f34f3284d62ec15c3
PolarXI2 <vinyciusxavier839@gmail.com>
2026-01-31 20:12:38 -0300

    Revise AssemblyInfo for AI translation feature
    Updated assembly information to reflect AI integration in the translation tool.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,25 +2,12 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// Informações Gerais (Identidade do seu Projeto)
 [assembly: AssemblyTitle("Tradutor de Mods Universal - Versão BR")]
-[assembly: AssemblyDescription("Ferramenta para tradução automática de mods e modpacks de Minecraft.")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Ferramenta com IA para tradução de mods e modpacks de Minecraft.")]
 [assembly: AssemblyCompany("PolarX12 Development")]
 [assembly: AssemblyProduct("Universal Mod Translator BR")]
 [assembly: AssemblyCopyright("Copyright © 2026 PolarX12")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Visibilidade e Acesso
 [assembly: ComVisible(false)]
-
-// Configurações de Tema do WPF (O que você já tinha)
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None,           // onde dicionários de temas específicos estão localizados
-    ResourceDictionaryLocation.SourceAssembly // onde dicionários genéricos estão localizados
-)]
-
-// Versão do Aplicativo
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
